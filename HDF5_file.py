2b911d8d6f924e49bf632183cd4992d8957a9781
Celine Armand <armand@lapth.cnrs.fr>
2018-06-04 17:25:48 +0200

    hdf5 code

---
+++ HDF5_file.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+
+import numpy as np
+
+# h5py with numpy >= 1.14 prints an error message on importing, suppress it
+np.warnings.filterwarnings('ignore')
+import h5py
+import sys
+import argparse
+
+
+def print_node(node, indent=4):
+    """Print string representation of a node of a tree
+
+    Args:
+        node: path in a tree
+        indent: the number of spaces to indent with each level
+
+    Returns:
+        string representation of this path
+
+    Examples:
+        >>> print_node("a/b/c")
+                c
+        >>> print_node("a/b")
+            b
+        >>> print_node("c")
+        c
+    """
+    *path, name = str(node).split('/')
+    print(' ' * indent * len(path) + name)
+
+
+def print_h5(h5filename, *, indent=4):
+    """Print the contents of an HDF5 file"""
+    h5file = h5py.File(h5filename)
+    h5file.visit(lambda node: print_node(node, indent=indent))
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Print contents of an HDF5 file.")
+    parser.add_argument("filename", help="HDF5 file name")
+    parser.add_argument("-i", "--indent", help="Indentation level",
+                        default=4, type=int)
+    args = parser.parse_args()
+
+    print_h5(args.filename, indent=args.indent)
+
+
+if __name__ == '__main__':
+    a = 42
+    "a is {:42}".format(a)
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+# import h5py
+#
+#
+# def print_node(node, indent = 4)->str: #-> None #not gonna print anything
+#
+#     """Print an indented representation of node
+#
+#     Args:
+#         node: Node to print
+#         indent: How many spaces par indent level
+#
+#     Returns:
+#         None
+#
+#         """
+#
+#     *first, last = str(node).split("/")
+#     print('   ' * len(first) + last)
+#
+#
+# def print_h5(h5filename, indent = 4):
+#     """for a given HDF5 file, print its content"""
+#     with h5py.File(h5filename) as h5file:
+#         h5file.visit(lambda node: print_node(node, indent=indent))
+#
+#
+# print_h5("/Users/celinearmand/ownCloud/PhD/School2018/HDF5_file.h5")
